d85fff9c9a5ef93e508db704ffa05181fee92313
Suyog Nerkar MC <suyogrnerkar@gmail.com>
2018-03-16 22:47:38 -0400

    JSDP: Object literal and namespace design patterns created.

--- chatapp/js/script.js
+++ chatapp/js/script.js
@@ -1,41 +1,66 @@
-var leadself = 'Me: ',
-		leadcomputer = "PC: ",
-		aSaid = ["This is a Cyber Chat"],
-		msgYes = "Yes, that's a great idea.",
-		msgNo = "No, that must be a mistake.",
-		aSassyStuff = ["Like mold on books, grow myths on history.",
-									"She moved like a poem and smiled like a sphinx.",
-									"As long as we don’t die, this is gonna be one hell of a story.",
-									"She laughed, and the desert sang.",
-									"You’ve got about as much charm as a dead slug."];
+/* 
+  //// NAMESPACE DESIGN PATTERN ////
+  - Problem Solved : Global Namespace Cluttering
 
-function talk(msg) {
-  echo(leadself + msg);
-}
+  - Create Long Namespaces so that we keep out of : 
+    Problems Arising Due To Name Conflicts For Functions And Variables
+*/
+var com = com || {};
+com.auxitrac = com.auxitrac || {};
+com.auxitrac.JSDP = com.auxitrac.JSDP || {};
 
-function replyYesNo() {
-  var msg = Math.random() > .5 ? msgYes : msgNo;
-  echo(leadcomputer + msg);
-}
+/*
+  //// OBJECT LITERAL DESIGN PATTERN ////
+  - Problem Solved : Global Namespace Cluttering
 
-function saySassyStuff() {
-  var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
-  echo(leadcomputer + msg);
-}
+  - Moving everything to Object from global space
+    as properties of an object.
+*/
 
-function echo(msg) {
-  aSaid.push("<div>" + msg + "</div>");
-  var aSaidLength = aSaid.length,
-    start = Math.max(aSaidLength - 6, 0),
-    out = "";
+com.auxitrac.JSDP.sudoChat = {
 
-  for (var i = start; i < aSaidLength; i++) {
-    out += aSaid[i];
+  leadself: 'Me: ',
+  aSaid: ["This is a Cyber Chat"],
+  leadcomputer: "PC: ",
+  msgYes: "Yes, that's a great idea.",
+  msgNo: "No, that must be a mistake.",
+  aSassyStuff: ["Like mold on books, grow myths on history.",
+                "She moved like a poem and smiled like a sphinx.",
+                "As long as we don’t die, this is gonna be one hell of a story.",
+                "She laughed, and the desert sang.",
+                "You’ve got about as much charm as a dead slug."],
+
+  talk: function(msg) {
+    this.echo(this.leadself + msg);
+  },
+
+  replyYesNo: function() {
+    var msg = Math.random() > .5 ? this.msgYes : this.msgNo;
+    this.echo(this.leadcomputer + msg);
+  },
+
+  saySassyStuff: function() {
+    var msg = this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length)];
+    this.echo(this.leadcomputer + msg);
+  },
+
+  echo: function(msg) {
+    this.aSaid.push("<div>" + msg + "</div>");
+
+    var aSaidLength = this.aSaid.length,
+      start = Math.max(aSaidLength - 6, 0),
+      out = "";
+
+    for (var i = start; i < aSaidLength; i++) {
+      out += this.aSaid[i];
+    }
+
+    $(".advert").html(out);
+    $("#talk span").text(msg);
   }
-  
-  $(".advert").html(out);
-  $("#talk span").text(msg);
+
 }
+
 
 
 
